c1eefab21524df1d5d2bf2e7ba4a4bfe4cf73e44
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-02-16 23:17:35 +0530

    Add files via upload

---
+++ Amazon_Clone/script.js
@@ -0,0 +1,7 @@
+//  let container = document.getElementById("scroll-Container");
+//  function scrollLeft(){
+//      container.scrollBy({left: -300, behavior: "smooth"});
+//  }
+//  function scrollRight(){
+//      container.scrollBy({left: 300, behavior: "smooth"});
+//  }
